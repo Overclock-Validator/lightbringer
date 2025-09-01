631ef118dfe25ae9178a885f36d74d04b968434b
rupansh <rupanshsekar@hotmail.com>
2025-09-01 21:38:39 +0530

    fix(rpc): fix deshreding

--- src/coding.rs
+++ src/coding.rs
@@ -1,10 +1,6 @@
 use std::collections::BTreeMap;
 
-use solana_entry::entry::Entry;
-use solana_ledger::shred::ShredType;
-use solana_sdk::stake::instruction::split;
-
-use crate::rpc::{deshred_to_entries, process_shreds_with_recovery};
+use crate::rpc::{deshred_to_entries, recover_shreds_and_group_by_completion};
 
 #[test]
 fn try_deshred() {
@@ -19,8 +15,7 @@ fn try_deshred() {
         use base64::{Engine, prelude::BASE64_STANDARD};
 
         let d = BASE64_STANDARD.decode(s).unwrap();
-        let s = Shred::new_from_serialized_shred(d).unwrap();
-        s
+        Shred::new_from_serialized_shred(d).unwrap()
     });
 
     let mut by_batch = HashMap::<u32, Vec<Shred>>::new();
@@ -34,7 +29,7 @@ fn try_deshred() {
     for (batch, shred) in by_batch {
         use crate::rpc::deshred_to_entries;
 
-        match deshred_to_entries(&shred) {
+        match deshred_to_entries(shred.iter()) {
             Ok(entries) => println!("batch entries {entries:?}"),
             Err(e) => println!("batch {batch} failed {e}"),
         }
@@ -43,7 +38,7 @@ fn try_deshred() {
 
 #[test]
 fn try_decode_and_deshred() {
-    use std::{collections::HashMap, fs};
+    use std::fs;
 
     use solana_ledger::shred::Shred;
 
@@ -54,75 +49,21 @@ fn try_decode_and_deshred() {
         use base64::{Engine, prelude::BASE64_STANDARD};
 
         let d = BASE64_STANDARD.decode(s).unwrap();
-        let s = Shred::new_from_serialized_shred(d).unwrap();
-        s
+        Shred::new_from_serialized_shred(d).unwrap()
     });
 
-    let mut by_batch = HashMap::<u32, Vec<Shred>>::new();
+    let mut by_batch = BTreeMap::<u32, Vec<Shred>>::new();
     for shred in shreds {
         by_batch
             .entry(shred.fec_set_index())
             .or_default()
             .push(shred);
     }
-    let slot = by_batch.values().next().unwrap()[0].slot();
 
-    let mut txn_cnt = 0usize;
-    let mut recovered_shreds = BTreeMap::<u32, Vec<Shred>>::new();
-    for (batch_index, shred_list) in by_batch {
-        let coding = shred_list.iter().find_map(|s| match s {
-            Shred::ShredCode(c) => Some(c.coding_header()),
-            Shred::ShredData(_) => None,
-        });
-        let existing_indexes = shred_list
-            .iter()
-            .filter(|s| s.shred_type() == ShredType::Data)
-            .map(|s| s.index())
-            .collect::<Vec<_>>();
-        log::info!(
-            "decoding {batch_index}, slot: {slot}, shreds_cnt {}, header: {coding:?}, existing data indices: {existing_indexes:?}",
-            shred_list.len()
-        );
-        let (new_data_shreds, _coding_shreds) = match process_shreds_with_recovery(shred_list) {
-            Ok(v) => v,
-            Err(e) => {
-                println!("recovery failed: batch {batch_index}, {e}");
-                continue;
-            }
-        };
-        log::info!("decoded {batch_index}, slot: {slot}, data_shreds: {new_data_shreds:?}");
-        recovered_shreds.insert(batch_index, new_data_shreds.clone());
-
-        // let entries = match deshred_to_entries(&new_data_shreds) {
-        //     Ok(entries) => {
-        //         println!("batch entries {entries:?}");
-        //         entries
-        //     },
-        //     Err(e) => {
-        //         println!("batch {batch_index} failed {e}");
-        //         continue;
-        //     },
-        // };
-        // txn_cnt += entries.iter().fold(0, |acc, e| acc + e.transactions.len());
-    }
-
-    let mut split_shreds = Vec::<Vec<Shred>>::new();
-    split_shreds.push(vec![]);
-    let mut cur_idx = 0;
-    for (_, shreds) in recovered_shreds {
-        split_shreds[cur_idx].extend_from_slice(&shreds);
-        let last = shreds.last().unwrap();
-        if last.data_complete() {
-            cur_idx += 1;
-            split_shreds.push(vec![]);
-        }
-    }
-
-    let mut entries = Vec::<Entry>::new();
-    for shred in split_shreds[..cur_idx].into_iter() {
-        println!("deshredding {:?}", shred);
-        let mut deshred = deshred_to_entries(&shred).unwrap();
-        entries.append(&mut deshred);
+    let mut entries = Vec::new();
+    for data_shreds in recover_shreds_and_group_by_completion(by_batch).unwrap() {
+        let mut deshred_entries = deshred_to_entries(data_shreds.values()).unwrap();
+        entries.append(&mut deshred_entries);
     }
 
     println!(
--- src/rpc/mod.rs
+++ src/rpc/mod.rs
@@ -3,7 +3,7 @@ use std::{collections::BTreeMap, net::SocketAddr};
 use base64::{Engine, prelude::BASE64_STANDARD};
 use ohkami::{IntoResponse, Json, Ohkami, Path, Response, Route, fang::Context};
 use solana_entry::entry::Entry;
-use solana_ledger::shred::{self, Shred, ShredType, Shredder};
+use solana_ledger::shred::{self, ReedSolomonCache, Shred, ShredType, Shredder};
 use thiserror::Error;
 
 use crate::store::shred::ShredStore;
@@ -41,43 +41,41 @@ pub struct DebugRpcInit {
     pub shred_store: ShredStore,
 }
 
-pub fn sort_shreds_by_type(shreds: Vec<Shred>) -> (Vec<Shred>, Vec<Shred>) {
-    let mut data_shreds: Vec<Shred> = vec![];
-    let mut coding_shreds: Vec<Shred> = vec![];
-
-    for s in shreds {
-        if s.shred_type() == ShredType::Data {
-            data_shreds.push(s);
-        } else {
-            coding_shreds.push(s);
+pub fn recover_shreds_and_group_by_completion(
+    shreds_by_fec_set_index: BTreeMap<u32, Vec<Shred>>,
+) -> Result<impl Iterator<Item = BTreeMap<u32, Shred>>, RpcError> {
+    let rs_cache = ReedSolomonCache::default();
+    let mut data_shreds_partitioned_by_completion = vec![BTreeMap::<u32, Shred>::new()];
+    for shred_batch in shreds_by_fec_set_index.into_values() {
+        let recovered =
+            shred::recover(shred_batch.clone(), &rs_cache).map_err(RpcError::ShredRecovery)?;
+        let last = data_shreds_partitioned_by_completion.last_mut().unwrap();
+        for shred in recovered {
+            let shred = shred.map_err(RpcError::ShredRecovery)?;
+            if shred.shred_type() != ShredType::Data {
+                continue;
+            }
+            last.insert(shred.index(), shred);
+        }
+        last.extend(
+            shred_batch
+                .into_iter()
+                .filter_map(|s| (s.shred_type() == ShredType::Data).then(|| (s.index(), s))),
+        );
+        if last.last_key_value().unwrap().1.data_complete() {
+            data_shreds_partitioned_by_completion.push(BTreeMap::new());
         }
     }
+    data_shreds_partitioned_by_completion.pop();
 
-    data_shreds.sort_by_key(|x| x.index());
-    coding_shreds.sort_by_key(|x| x.index());
-
-    (data_shreds, coding_shreds)
+    Ok(data_shreds_partitioned_by_completion.into_iter())
 }
 
-pub fn process_shreds_with_recovery(
-    shreds: Vec<Shred>,
-) -> Result<(Vec<Shred>, Vec<Shred>), RpcError> {
-    // Perform recovery on shreds
-    let recovery: Vec<Shred> = shred::recover(shreds.clone(), &Default::default())
-        .map_err(RpcError::ShredRecovery)?
-        .collect::<Result<Vec<_>, _>>()
-        .map_err(RpcError::ShredRecovery)?;
-
-    // Combine original and recovered shreds
-    let all_shreds: Vec<Shred> = [shreds, recovery].concat();
-
-    // Sort into data and coding shreds
-    Ok(sort_shreds_by_type(all_shreds))
-}
-
-pub fn deshred_to_entries(data_shreds: &[Shred]) -> Result<Vec<Entry>, RpcError> {
-    let shreds = data_shreds.iter().map(Shred::payload);
-    let deshred_payload = Shredder::deshred(shreds).map_err(RpcError::Deshred)?;
+pub fn deshred_to_entries<'a>(
+    data_shreds: impl Iterator<Item = &'a Shred>,
+) -> Result<Vec<Entry>, RpcError> {
+    let deshred_payload =
+        Shredder::deshred(data_shreds.map(|s| s.payload())).map_err(RpcError::Deshred)?;
     let deshred_entries: Vec<Entry> = bincode::deserialize(&deshred_payload)?;
     Ok(deshred_entries)
 }
@@ -87,11 +85,12 @@ async fn get_slot(
     Path(slot): Path<u64>,
 ) -> Result<Json<Vec<Entry>>, RpcError> {
     let shred_store = shred_store.0.clone();
-    // TODO: this blocks the current thread, we should probably implement a separate thread for fjall
-    // spawn_blocking can't be used with ohkami right now :|
-    let unsorted_shreds = shred_store.get_slot_shreds(slot)?;
+
+    let exec = glommio::executor();
+    let unsorted_shreds = exec
+        .spawn_blocking(move || shred_store.get_slot_shreds(slot))
+        .await?;
     let mut shreds_for_slot = BTreeMap::<u32, Vec<Shred>>::new();
-    log::info!("found {} shreds for slot", unsorted_shreds.len());
 
     for shred in unsorted_shreds {
         let deser =
@@ -101,74 +100,14 @@ async fn get_slot(
             .or_default()
             .push(deser);
     }
-    let mut entries = Vec::<Entry>::new();
-    for (batch_index, shred_list) in shreds_for_slot {
-        let coding = shred_list.iter().find_map(|s| match s {
-            Shred::ShredCode(c) => Some(c.coding_header()),
-            Shred::ShredData(_) => None,
-        });
-        let existing_indexes = shred_list
-            .iter()
-            .filter(|s| s.shred_type() == ShredType::Data)
-            .map(|s| s.index())
-            .collect::<Vec<_>>();
-        log::info!(
-            "decoding {batch_index}, slot: {slot}, shreds_cnt {}, header: {coding:?}, existing data indices: {existing_indexes:?}",
-            shred_list.len()
-        );
-        let (data_shreds, _coding_shreds) = process_shreds_with_recovery(shred_list)?;
-        log::info!(
-            "deshreding {batch_index}, slot: {slot}, shreds {:?}",
-            data_shreds
-        );
-        let mut new_entries = deshred_to_entries(&data_shreds).map_err(|e| {
-            log::info!("deshredding {batch_index}, slot: {slot} failed, {e}");
-            e
-        })?;
-        entries.append(&mut new_entries);
+
+    let mut entries = Vec::new();
+    for data_shreds in recover_shreds_and_group_by_completion(shreds_for_slot)? {
+        let mut deshred_entries = deshred_to_entries(data_shreds.values())?;
+        entries.append(&mut deshred_entries);
     }
 
     Ok(Json(entries))
-}
-
-async fn get_data_shreds_sorted(
-    shred_store: Context<'_, ShredStore>,
-    Path(slot): Path<u64>,
-) -> Result<Json<Vec<String>>, RpcError> {
-    let shred_store = shred_store.0.clone();
-    // TODO: this blocks the current thread, we should probably implement a separate thread for fjall
-    // spawn_blocking can't be used with ohkami right now :|
-    let unsorted_shreds = shred_store.get_slot_shreds(slot)?;
-    let mut shreds_for_slot = BTreeMap::<u32, Vec<Shred>>::new();
-    log::info!("found {} shreds for slot", unsorted_shreds.len());
-
-    for shred in unsorted_shreds {
-        let deser =
-            Shred::new_from_serialized_shred(shred.to_vec()).map_err(RpcError::ShredDeser)?;
-        shreds_for_slot
-            .entry(deser.fec_set_index())
-            .or_default()
-            .push(deser);
-    }
-    let mut data_shreds = Vec::<String>::new();
-    for (batch_index, shred_list) in shreds_for_slot {
-        let coding = shred_list.iter().find_map(|s| match s {
-            Shred::ShredCode(c) => Some(c.coding_header()),
-            Shred::ShredData(_) => None,
-        });
-        log::info!(
-            "decoding {batch_index}, slot: {slot}, shreds_cnt {}, header: {coding:?}",
-            shred_list.len()
-        );
-        let (new_data_shreds, _coding_shreds) = process_shreds_with_recovery(shred_list)?;
-        data_shreds.extend(
-            new_data_shreds
-                .into_iter()
-                .map(|s| BASE64_STANDARD.encode(s.payload())),
-        );
-    }
-
-    Ok(Json(data_shreds))
 }
 
 async fn get_stored_shreds_raw(
@@ -188,7 +127,6 @@ pub async fn debug_rpc_listener(init: DebugRpcInit) {
     Ohkami::new((
         Context::new(init.shred_store),
         "/slot_entries/:slot".GET(get_slot),
-        "/raw_slot_entries/:slot".GET(get_data_shreds_sorted),
         "/stored_shreds/:slot".GET(get_stored_shreds_raw),
     ))
     .howl(init.listen_addr)
