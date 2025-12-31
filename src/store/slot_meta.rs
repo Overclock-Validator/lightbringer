bf9f55564ef62aae0d7d6957dea2fde83bedd3c7
rupansh <rupanshsekar@hotmail.com>
2025-12-31 17:10:59 +0530

    chore(slot_meta): remove debug logs

--- src/store/slot_meta.rs
+++ src/store/slot_meta.rs
@@ -49,13 +49,6 @@ impl SlotMetadata {
 
     fn calculate_missing_shreds_bounded(&self) -> Option<Vec<u32>> {
         let max_batch_index = self.required_batches? * DATA_SHREDS_PER_FEC_SET;
-        for (batch_idx, batch_idxs) in self.fec_data_map.iter() {
-            log::info!("bounded missing shreds: batch {batch_idx} has shreds {batch_idxs:?}");
-        }
-        log::info!(
-            "bounded missing shreds: required batches = {:?}",
-            self.required_batches
-        );
 
         let required_shreds = (0..max_batch_index as u32)
             .step_by(DATA_SHREDS_PER_FEC_SET)
